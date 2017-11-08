0a42f5fccaec7f9e5b43c08dbf41748a52e22763
Jeffrey Schultz <jeffrey.l.schultz@caci.com>
2017-11-08 13:59:09 -0500

    Commit of changes to provide a basic logging implementation that outputs to the Console.

--- src/Spring/Spring.Core/Logging/LogManager.cs
+++ src/Spring/Spring.Core/Logging/LogManager.cs
@@ -4,24 +4,219 @@ namespace Spring.Logging
 {
     public class LogManager
     {
+        protected class ConsoleLogger : ILogger
+        {
+            public void Trace(object message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void Trace(object message, Exception exception)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void TraceFormat(string format, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void TraceFormat(string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void TraceFormat(IFormatProvider formatProvider, string format, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void TraceFormat(IFormatProvider formatProvider, string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void Debug(object message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void Debug(object message, Exception exception)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void DebugFormat(string format, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void DebugFormat(string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void DebugFormat(IFormatProvider formatProvider, string format, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void DebugFormat(IFormatProvider formatProvider, string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void Info(object message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void Info(object message, Exception exception)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void InfoFormat(string format, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void InfoFormat(string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void InfoFormat(IFormatProvider formatProvider, string format, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void InfoFormat(IFormatProvider formatProvider, string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void Warn(object message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void Warn(object message, Exception exception)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void WarnFormat(string format, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void WarnFormat(string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void WarnFormat(IFormatProvider formatProvider, string format, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void WarnFormat(IFormatProvider formatProvider, string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void Error(object message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void Error(object message, Exception exception)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void ErrorFormat(string format, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void ErrorFormat(string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void ErrorFormat(IFormatProvider formatProvider, string format, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void ErrorFormat(IFormatProvider formatProvider, string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(String.Format(formatProvider, format, args));
+            }
+
+            public void Fatal(object message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void Fatal(object message, Exception exception)
+            {
+                Console.WriteLine(message);
+            }
+
+            public void FatalFormat(string format, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void FatalFormat(string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(format, args);
+            }
+
+            public void FatalFormat(IFormatProvider formatProvider, string format, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public void FatalFormat(IFormatProvider formatProvider, string format, Exception exception, params object[] args)
+            {
+                Console.WriteLine(string.Format(formatProvider, format, args));
+            }
+
+            public bool IsTraceEnabled { get; } = true;
+
+            public bool IsDebugEnabled { get; } = true;
+
+            public bool IsErrorEnabled { get; } = true;
+
+            public bool IsFatalEnabled { get; } = true;
+
+            public bool IsInfoEnabled { get; } = true;
+
+            public bool IsWarnEnabled { get; } = true;
+        }
+
         public static ILogger GetLogger<T>()
         {
-            throw new NotImplementedException();
+            return new ConsoleLogger();
         }
 
         public static ILogger GetLogger(Type type)
         {
-            throw new NotImplementedException();
+            return new ConsoleLogger();
         }
 
         public static ILogger GetCurrentClassLogger()
         {
-            throw new NotImplementedException();
+            return new ConsoleLogger();
         }
 
         public static ILogger GetLogger(string fullName)
         {
-            throw new NotImplementedException();
+            return new ConsoleLogger();
         }
     }
 }
